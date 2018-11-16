8e04cce598a5203f521f28d541de88c496eefce2
miipel <mpeltotalo@gmail.com>
2018-11-16 09:16:52 +0200

    Deploy app to Heroku

---
+++ build/precache-manifest.0412c1bf55458adb8a48a01db9ffe9de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e86880ae362ffcb5095f",
+    "url": "/static/js/main.e86880ae.chunk.js"
+  },
+  {
+    "revision": "75866e743f7fe4de3c33",
+    "url": "/static/js/1.75866e74.chunk.js"
+  },
+  {
+    "revision": "e86880ae362ffcb5095f",
+    "url": "/static/css/main.53b0bc69.chunk.css"
+  },
+  {
+    "revision": "fd8ec34fc6783fc154e2c5fb75e612c5",
+    "url": "/index.html"
+  }
+];
