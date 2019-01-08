cb6d5304a9540639c16d52f98e860a5d452faab6
miipel <mpeltotalo@gmail.com>
2019-01-08 16:58:29 +0200

    Task 3.16 completed

--- index.js
+++ index.js
@@ -1,105 +1,102 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const Person = require('./models/person');
+const Person = require('./models/person')
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('build'));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
 morgan.token('data', (req, res) => {
-  return JSON.stringify(req.body);
-});
+  return JSON.stringify(req.body)
+})
 app.use(
   morgan(':method :url :data :status :res[content-length] - :response-time ms')
-);
+)
 
 let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-    name: 'Martti Tienari',
-    number: '040-123456',
-    id: 2
-  },
-  {
-    name: 'Arto Järvinen',
-    number: '040-123456',
-    id: 3
-  },
-  {
-    name: 'Lea Kutvonen',
-    number: '040-123456',
-    id: 4
-  }
-];
+  // {
+  //   name: 'Arto Hellas',
+  //   number: '040-123456',
+  //   id: 1
+  // },
+  // {
+  //   name: 'Martti Tienari',
+  //   number: '040-123456',
+  //   id: 2
+  // },
+  // {
+  //   name: 'Arto Järvinen',
+  //   number: '040-123456',
+  //   id: 3
+  // },
+  // {
+  //   name: 'Lea Kutvonen',
+  //   number: '040-123456',
+  //   id: 4
+  // }
+]
 
 app.get('/', (request, response) => {
-  response.send('<p>Jaahas</p>');
-});
+  response.send('<p>Jaahas</p>')
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons.map(Person.format));
-  });
-});
+    response.json(persons.map(Person.format))
+  })
+})
 
 app.get('/api/persons/:id', (request, response) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
-      response.json(Person.format(person));
+      response.json(Person.format(person))
     } else {
-      response.status(404).end();
+      response.status(404).end()
     }
-  });
-});
+  })
+})
 
 app.get('/info', (request, response) => {
-  const date = `<p>${new Date()}</p>`;
-  const info = `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>`;
-  response.send(info + date);
-});
-
-const generateRandomId = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+  const date = `<p>${new Date()}</p>`
+  const info = `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p>`
+  response.send(info + date)
+})
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body;
+  const body = request.body
 
   if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ error: 'name or number missing' });
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ error: 'name already exists' });
-  }
+  // if (persons.find(person => person.name === body.name)) {
+  //   return response.status(400).json({ error: 'name already exists' })
+  // }
 
   const person = new Person({
     name: body.name,
     number: body.number
-  });
+  })
 
   person.save().then(savedPerson => {
-    response.json(Person.format(savedPerson));
-  });
-});
+    response.json(Person.format(savedPerson))
+  })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
+  try {
+    Person.findOneAndDelete({ _id: request.params.id }).then(res => {
+      response.status(204).end()
+    })
+  } catch (e) {
+    console.log(e)
+  }
+})
 
-  response.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose.connect(
   url,
   { useNewUrlParser: true }
-);
+)
 
-let personSchema = new Schema({ name: String, number: String, id: String });
+let personSchema = new Schema({ name: String, number: String, id: String })
 
 personSchema.statics.format = function(person) {
   return {
     name: person.name,
     number: person.number,
     id: person._id
-  };
-};
+  }
+}
 
-let Person = mongoose.model('Person', personSchema);
+let Person = mongoose.model('Person', personSchema)
 
-module.exports = Person;
+module.exports = Person
